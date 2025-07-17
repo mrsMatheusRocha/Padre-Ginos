a172b8e246608d6dd63b6fd510fb695e6e9edfd3
Matheus Rocha Sousa <166670004+Marthplays@users.noreply.github.com>
2025-07-17 17:16:30 -0300

    fix: usePizzaOfTheDay fetch fixed

--- src/hooks/usePizzaOfTheDay.js
+++ src/hooks/usePizzaOfTheDay.js
@@ -5,7 +5,8 @@ export default function usePizzaOfTheDay() {
 
   useEffect(() => {
     async function fetchPizzaOfTheDay() {
-      const res = await fetch("/api/pizza-of-the-day");
+      const apiUrl = import.meta.env.VITE_API_URL;
+      const res = await fetch(`${apiUrl}/api/pizza-of-the-day`);
       const data = await res.json();
       setPizzaOfTheDay(data);
     }
