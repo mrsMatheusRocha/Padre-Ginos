45f1c9e1609cdc53648f0d3ed690b37b1fe68317
Matheus Rocha Sousa <166670004+Marthplays@users.noreply.github.com>
2025-07-17 02:10:33 -0300

    feat: Implemented tests with Vitest and Playwright
    - React Router and React Query setup
    - API interaction for fetching pizza data, submitting contact forms, and managing orders
    - Unit tests for components

---
+++ src/__tests__/contact.lazy.node.test.jsx
@@ -0,0 +1,50 @@
+import { render } from "@testing-library/react";
+import { expect, test, vi } from "vitest";
+import createFetchMock from "vitest-fetch-mock";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { Route } from "../routes/contact.lazy";
+
+const queryClient = new QueryClient();
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
+
+test("can submit contact form", async () => {
+  fetchMocker.mockResponse(JSON.stringify({ status: "ok" }));
+  const screen = render(
+    <QueryClientProvider client={queryClient}>
+      <Route.options.component />
+    </QueryClientProvider>
+  );
+
+  const nameInput = screen.getByPlaceholderText("Name");
+  const emailInput = screen.getByPlaceholderText("Email");
+  const messageInput = screen.getByPlaceholderText("Message");
+
+  const testData = {
+    name: "Matheus",
+    email: "test@example.com",
+    message: "This is test",
+  };
+
+  nameInput.value = testData.name;
+  emailInput.value = testData.email;
+  messageInput.value = testData.message;
+
+  const button = screen.getByRole("button");
+  button.click();
+
+  const h3 = await screen.findByRole("heading", {level: 3});
+
+  expect(h3.innerText).toContain("Submitted");
+  const requests = fetchMocker.requests();
+  expect(requests.length).toBe(1);
+  expect(requests[0].url).toBe("/api/contact");
+  expect(fetchMocker).toHaveBeenCalledWith("/api/contact", {
+    body: JSON.stringify(testData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  });
+});
